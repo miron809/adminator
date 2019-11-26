f719b1c33a583666f7bca206382514cff4fa76d9
Dmitriy Mironenko <dim@uain.su>
2019-11-26 15:06:01 +0200

    added empty templates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainLayoutComponent } from './shared/components/main-layout/main-layout.component';
 import { SkyconsModule } from 'ngx-skycons';
+import { DashboardPageComponent } from './dashboard-page/dashboard-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { SignUpPageComponent } from './sign-up-page/sign-up-page.component';
+import { ComposePageComponent } from './compose-page/compose-page.component';
+import { EmailPageComponent } from './email-page/email-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainLayoutComponent
+    MainLayoutComponent,
+    DashboardPageComponent,
+    LoginPageComponent,
+    SignUpPageComponent,
+    ComposePageComponent,
+    EmailPageComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/compose-page/compose-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-compose-page',
+  templateUrl: './compose-page.component.html',
+  styleUrls: ['./compose-page.component.scss']
+})
+export class ComposePageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
