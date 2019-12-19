15b99cb23291538da8890dafe5c99cee82b000d5
Dmitriy Mironenko <dim@uain.su>
2019-12-19 16:59:41 +0200

    updated chat widget

--- src/app/widgets/chat/chat.component.ts
+++ src/app/widgets/chat/chat.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+  ViewChildren
+} from '@angular/core';
 import { Message } from './shared/model/message';
 import { User } from './shared/model/user';
 import { Action } from './shared/model/action';
@@ -9,16 +16,38 @@ import { ChatService } from '../../shared/services/chat.service';
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.scss']
 })
-export class ChatComponent implements OnInit {
+export class ChatComponent implements OnInit, AfterViewInit {
   action = Action;
   user: User;
   messages: Message[] = [];
   messageContent: string;
 
+  // getting a reference to the overall list, which is the parent container of the list items
+  @ViewChild('chatBody', { static: false }) chatBody: ElementRef;
+
+  // getting a reference to the items/messages within the list
+  @ViewChildren('chatItem', { read: ElementRef }) chatItem;
+
   constructor(private chatService: ChatService) { }
 
   ngOnInit(): void {
     this.messages = this.chatService.messageHistory;
+  }
+
+  ngAfterViewInit(): void {
+    // subscribing to any changes in the list of items / messages
+    this.chatItem.changes.subscribe(elements => {
+      this.scrollToBottom();
+    });
+  }
+
+  // auto-scroll fix: inspired by this stack overflow post
+  // https://stackoverflow.com/questions/35232731/angular2-scroll-to-bottom-chat-style
+  private scrollToBottom(): void {
+    try {
+      this.chatBody.nativeElement.scrollTop = this.chatBody.nativeElement.scrollHeight;
+    } catch (err) {
+    }
   }
 
   sendMessage(message: string): void {
