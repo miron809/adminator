5099c2571ed647dd78f4dbb20f65e16688e91c0b
Dmitriy Mironenko <dim@uain.su>
2020-01-15 17:47:19 +0200

    added spinner to email-list

--- src/app/email-page/email-list/email-list.component.ts
+++ src/app/email-page/email-list/email-list.component.ts
@@ -2,6 +2,7 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Email } from '../../shared/interfaces';
 import { EmailService } from '../../shared/services/email.service';
 import { filter } from 'rxjs/operators';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-email-list',
@@ -16,21 +17,26 @@ export class EmailListComponent implements OnInit {
   public hightlightStatus = [];
 
   constructor(
-    private emailService: EmailService
-  ) { }
+    private emailService: EmailService,
+    private spinner: NgxSpinnerService
+  ) {
+  }
 
   ngOnInit() {
     this.getEmails();
   }
 
   getEmails() {
+    this.spinner.show();
     this.emailService.getEmails()
       .subscribe(emails => {
-        this.emails = Object.values(emails);
-        this.emails.sort( (a: any, b: any) => {
-          return (Date.parse(b.date) - Date.parse(a.date));
-        });
-      });
+          this.emails = Object.values(emails);
+          this.emails.sort((a: any, b: any) => {
+            return (Date.parse(b.date) - Date.parse(a.date));
+          });
+          this.spinner.hide();
+        },
+        () => this.spinner.hide());
   }
 
   focusEmail(email, i) {
